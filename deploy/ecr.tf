f5ab9ea8c0536273e09d109b0b3da8bc51645b25
Andrew Yang <ddvkid@gmail.com>
2022-01-05 17:14:50 +1100

    ecr secret

--- deploy/ecr.tf
+++ deploy/ecr.tf
@@ -7,27 +7,27 @@ resource "aws_ecr_repository" "order" {
   }
 }
 
-#data "aws_ecr_authorization_token" "token" {
-#}
-#
-#resource "kubernetes_secret" this {
-#  metadata {
-#    namespace = var.k8s_namespace
-#    name = "ecr-secret"
-#  }
-#
-#  data = {
-#    ".dockerconfigjson" = jsonencode({
-#      auths = {
-#        (var.image_registry) = {
-#          auth = data.aws_ecr_authorization_token.token.authorization_token
-#        }
-#      }
-#    })
-#  }
-#
-#  type = "kubernetes.io/dockerconfigjson"
-#  lifecycle {
-#    ignore_changes = [data]
-#  }
-#}
+data "aws_ecr_authorization_token" "token" {
+}
+
+resource "kubernetes_secret" this {
+  metadata {
+    namespace = var.k8s_namespace
+    name = "ecr-secret"
+  }
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        (var.image_registry) = {
+          auth = data.aws_ecr_authorization_token.token.authorization_token
+        }
+      }
+    })
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+  lifecycle {
+    ignore_changes = [data]
+  }
+}
