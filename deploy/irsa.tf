4f4efaf73994b5806a7f8d1c8b12a23f595318d7
Andrew Yang <ddvkid@gmail.com>
2022-01-17 18:43:19 +1100

    attach kakfa cluster policy

--- deploy/irsa.tf
+++ deploy/irsa.tf
@@ -22,7 +22,29 @@ resource "aws_iam_role" "irsa" {
   })
 }
 
+resource "aws_iam_policy" "kafka_cluster" {
+  name        = "${local.cluster_name}-policy-kafka_cluster"
+  description = "Policy for kafka cluster"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "kafka-cluster:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
 resource "aws_iam_role_policy_attachment" "irsa" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonMSKFullAccess"
+  role       = aws_iam_role.irsa.name
+}
+
+resource "aws_iam_role_policy_attachment" "kafka" {
+  policy_arn = aws_iam_policy.kafka_cluster.arn
   role       = aws_iam_role.irsa.name
 }
